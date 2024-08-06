2f40651c7fa982a42e6488e9bb379d6e791b8342
Carlo Castoldi <carlo.castoldi@outlook.com>
2024-08-06 12:10:08 +0200

    adapt extension to QP 0.6.* image lazy loading

--- src/main/java/qupath/ext/braian/ImageChannelTools.java
+++ src/main/java/qupath/ext/braian/ImageChannelTools.java
@@ -9,9 +9,11 @@ import ij.ImagePlus;
 import ij.process.ImageProcessor;
 import ij.process.ImageStatistics;
 import qupath.imagej.tools.IJTools;
+import qupath.lib.images.ImageData;
 import qupath.lib.images.PathImage;
 import qupath.lib.images.servers.ImageChannel;
 import qupath.lib.images.servers.ImageServer;
+import qupath.lib.images.servers.ImageServerMetadata;
 import qupath.lib.objects.hierarchy.PathObjectHierarchy;
 import qupath.lib.regions.RegionRequest;
 
@@ -26,6 +28,7 @@ class IllegalChannelName extends RuntimeException {
 
 public class ImageChannelTools {
     private final String name;
+    private final ImageData<BufferedImage> imageData;
     private final ImageServer<BufferedImage> server;
     private final int nChannel;
 
@@ -36,13 +39,38 @@ public class ImageChannelTools {
      */
     public ImageChannelTools(String name, ImageServer<BufferedImage> server) {
         this.name = name;
+        this.imageData = null;
         this.server = server;
         this.nChannel = this.findNChannel();
     }
 
+    /**
+     * Crates an {@link ImageChannelTools} from the given channel name and {@link ImageServer}
+     * @param name name of the channel
+     * @param imageData data of the image to which the channel is referring to
+     */
+    public ImageChannelTools(String name, ImageData<BufferedImage> imageData) {
+        this.name = name;
+        this.imageData = imageData;
+        this.server = null;
+        this.nChannel = this.findNChannel();
+    }
+
+    private ImageServer<BufferedImage> getServer() {
+        if (this.server != null)
+            return this.server;
+        return this.imageData.getServer();
+    }
+
+    private ImageServerMetadata getMetadata() {
+        if (this.server != null)
+            return this.server.getMetadata();
+        return this.imageData.getServerMetadata();
+    }
+
     private int findNChannel() {
         int nChannel = 0;
-        for (ImageChannel ch: this.server.getMetadata().getChannels()) {
+        for (ImageChannel ch: this.getMetadata().getChannels()) {
             if(ch.getName().equals(this.name))
                 return nChannel;
             nChannel++;
@@ -80,9 +108,10 @@ public class ImageChannelTools {
      * @see ImageServer#getDownsampleForResolution(int)
      */
     public ImageProcessor getImageProcessor(int resolutionLevel) throws IOException {
-        double downsample = this.server.getDownsampleForResolution(Math.min(this.server.nResolutions()-1, resolutionLevel));
-        RegionRequest request = RegionRequest.createInstance(this.server, downsample);
-        PathImage<ImagePlus> pathImage = IJTools.convertToImagePlus(this.server, request);
+        ImageServer<BufferedImage> server = this.getServer();
+        double downsample = server.getDownsampleForResolution(Math.min(server.nResolutions()-1, resolutionLevel));
+        RegionRequest request = RegionRequest.createInstance(server, downsample);
+        PathImage<ImagePlus> pathImage = IJTools.convertToImagePlus(server, request);
         ImagePlus ci = pathImage.getImage();
 
         int ijChannel = this.nChannel+1; // ij.CompositeImage uses 1-based channels
